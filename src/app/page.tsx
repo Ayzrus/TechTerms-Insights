50ec90e1f99368d95f5c83a7892f69fb066fd747
Rodrigo Carvalho <159010685+Ayzrus@users.noreply.github.com>
2025-01-16 08:21:24 +0000

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,172 +9,172 @@ const terms = [
     definition:
       "Habilidades interpessoais que incluem comunicação, empatia, trabalho em equipe e resolução de problemas.",
     id: "SS",
-    image: "/images/ss.jpg",
+    image: "./images/ss.jpg",
   },
   {
     term: "CRM",
     definition:
       "Sistema para gestão de relação com clientes, ajudando a melhorar as vendas, marketing e serviço ao cliente.",
     id: "CRM",
-    image: "/images/crm.jpg",
+    image: "./images/crm.jpg",
   },
   {
     term: "ERP",
     definition:
       "Sistema integrado que gerencia processos de negócio, como contabilidade, compras e produção.",
     id: "ERP",
-    image: "/images/erp.jpg",
+    image: "./images/erp.jpg",
   },
   {
     term: "MES",
     definition:
       "Sistema que controla e monitora o processo de produção em tempo real.",
     id: "MES",
-    image: "/images/mes.jpg",
+    image: "./images/mes.jpg",
   },
   {
     term: "SCM",
     definition:
       "Gestão da cadeia de fornecimento, desde a produção até a entrega do produto ao consumidor final.",
     id: "SCM",
-    image: "/images/scm.jpg",
+    image: "./images/scm.jpg",
   },
   {
     term: "WMS",
     definition: "Sistema para gerenciar operações de armazém e estoque.",
     id: "WMS",
-    image: "/images/wms.jpg",
+    image: "./images/wms.jpg",
   },
   {
     term: "HRM",
     definition:
       "Gestão de recursos humanos, incluindo recrutamento, formação e desenvolvimento dos colaboradores.",
     id: "HRM",
-    image: "/images/hrm.jpg",
+    image: "./images/hrm.jpg",
   },
   {
     term: "Big Data",
     definition:
       "Conjunto de dados muito grandes ou complexos para serem tratados por métodos tradicionais, exigindo técnicas avançadas de análise.",
     id: "BigData",
-    image: "/images/bigdata.jpg",
+    image: "./images/bigdata.jpg",
   },
   {
     term: "Cybersecurity",
     definition:
       "Prática de proteger sistemas, redes e programas contra ataques digitais.",
     id: "Cybersecurity",
-    image: "/images/cybersecurity.jpg",
+    image: "./images/cybersecurity.jpg",
   },
   {
     term: "IA (Inteligência Artificial)",
     definition:
       "Tecnologias que permitem que máquinas imitem a inteligência humana, como aprendizado e tomada de decisões.",
     id: "IA",
-    image: "/images/ia.jpg",
+    image: "./images/ia.jpg",
   },
   {
     term: "IoT (Internet of Things)",
     definition:
       "Rede de dispositivos físicos conectados à internet, que coletam e compartilham dados.",
     id: "IoT",
-    image: "/images/iot.jpg",
+    image: "./images/iot.jpg",
   },
   {
     term: "IIoT (Industrial Internet of Things)",
     definition:
       "Aplicativo da IoT no setor industrial para melhorar a eficiência e reduzir custos.",
     id: "IIoT",
-    image: "/images/iiot.jpg",
+    image: "./images/iiot.jpg",
   },
   {
     term: "AR (Augmented Reality)",
     definition:
       "Tecnologia que sobrepõe elementos digitais ao mundo real, melhorando a experiência do usuário.",
     id: "AR",
-    image: "/images/ar.jpg",
+    image: "./images/ar.jpg",
   },
   {
     term: "VR (Virtual Reality)",
     definition:
       "Simulação de um ambiente tridimensional totalmente virtual, permitindo a imersão do usuário.",
     id: "VR",
-    image: "/images/vr.jpg",
+    image: "./images/vr.jpg",
   },
   {
     term: "B2B (Business to Business)",
     definition: "Transações comerciais entre empresas.",
     id: "B2B",
-    image: "/images/b2b.jpg",
+    image: "./images/b2b.jpg",
   },
   {
     term: "B2C (Business to Consumer)",
     definition: "Transações comerciais entre empresas e consumidores finais.",
     id: "B2C",
-    image: "/images/b2c.jpg",
+    image: "./images/b2c.jpg",
   },
   {
     term: "H2H (Human to Human)",
     definition:
       "Enfoque de negócios que valoriza a conexão humana em todas as interações comerciais.",
     id: "H2H",
-    image: "/images/h2h.jpg",
+    image: "./images/h2h.jpg",
   },
   {
     term: "NFC (Near Field Communication)",
     definition:
       "Tecnologia de comunicação sem fio de curto alcance, usada para pagamentos e transferências de dados.",
     id: "NFC",
-    image: "/images/nfc.jpg",
+    image: "./images/nfc.jpg",
   },
   {
     term: "RFID (Radio Frequency Identification)",
     definition:
       "Tecnologia que utiliza ondas de rádio para identificar e rastrear objetos.",
     id: "RFID",
-    image: "/images/rfid.jpg",
+    image: "./images/rfid.jpg",
   },
   {
     term: "Bar Code",
     definition:
       "Código de barras, uma representação visual de dados legível por máquinas, usado para identificar produtos.",
     id: "BarCode",
-    image: "/images/barcode.jpg",
+    image: "./images/barcode.jpg",
   },
   {
     term: "QR Code",
     definition:
       "Código de resposta rápida, que pode armazenar mais informações do que o código de barras e ser lido por smartphones.",
     id: "QRCode",
-    image: "/images/qrcode.jpg",
+    image: "./images/qrcode.jpg",
   },
   {
     term: "Blockchain",
     definition:
       "Tecnologia de registro distribuído que garante a segurança e transparência das transações digitais.",
     id: "Blockchain",
-    image: "/images/blockchain.jpg",
+    image: "./images/blockchain.jpg",
   },
   {
     term: "Data Science",
     definition:
       "Campo que utiliza métodos científicos, processos e sistemas para extrair informações de dados estruturados e não estruturados.",
     id: "DataScience",
-    image: "/images/datascience.jpg",
+    image: "./images/datascience.jpg",
   },
   {
     term: "Cloud Computing",
     definition:
       "Uso de servidores remotos na internet para armazenar, gerenciar e processar dados, em vez de um servidor local ou um computador pessoal.",
     id: "CloudComputing",
-    image: "/images/cloudcomputing.jpg",
+    image: "./images/cloudcomputing.jpg",
   },
   {
     term: "Deep Learning",
     definition:
       "Subconjunto de aprendizado de máquina que utiliza redes neurais artificiais para modelar e resolver problemas complexos.",
     id: "DeepLearning",
-    image: "/images/deeplearning.jpg",
+    image: "./images/deeplearning.jpg",
   },
 ];
 
